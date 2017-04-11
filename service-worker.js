655af2356d360f57f5b8cabf7ed120082f31757d
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2017-04-11 18:05:52 -0300

    Add page base + service-worker to handle cache

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+let name = 'book-market-cache';
+let files = [
+  './',
+  './index.html',
+  './style.css',
+  './main.js'
+];
+
+console.log(caches);
+
+self.addEventListener('install', event => {
+  let cacheFiles = () => caches
+    .open(name)
+    .then(cache => cache.addAll(files))
+    .catch(error => console.error(error));
+
+  event.waitUntil(cacheFiles());
+});
+
+self.addEventListener('activate', event => {
+  let cacheDeleteUnused = () => caches
+    .keys()
+    .then(list => Promise.all(list.map(key => {
+      if (key !== name)
+        return caches.delete(key)
+      })))
+    .catch(error => console.error(error));
+
+  event.waitUntil(cacheDeleteUnused());
+
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  let cacheFetch = () => caches
+    .match(event.request)
+    .then(response => response || fetch(event.request))
+    .catch(error => console.error(error));
+
+  event.respondWith(cacheFetch());
+});
