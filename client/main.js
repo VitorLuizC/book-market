2eec19e5a7566db3d66f284b94ef36354f090e80
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2017-04-12 18:25:39 -0300

    Add save book + fetch books (undone)

--- client/main.js
+++ client/main.js
@@ -1,3 +1,36 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js');
+if (!'serviceWorker' in navigator) // Caso o browser dê suporte a service workers
+  navigator.serviceWorker         // o registro e deixo ele lidar com o browser.
+    .register('./service-worker.js')
+
+document.querySelector('#button-add').addEventListener('click', addBook)
+
+/**
+ * @param {Event} event
+ */
+function addBook(event) {
+  const book = {
+    name: document.querySelector('#book-name').value || '',
+    year: +(document.querySelector('#book-year').value) || 0,
+    author: document.querySelector('#book-author').value || ''
+  }
+
+  const owner = {
+    name: document.querySelector('#owner-name').value || '',
+    email: document.querySelector('#owner-email').value || '',
+    phone: document.querySelector('#owner-phone').value || ''
+  }
+
+  fetch('/book', {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ book, owner })
+  })
+    .then(() => alert('Aê, parabéns!'))
+    .catch(error => alert('Se fodeu!'))
+
+  event.preventDefault()
+  event.stopPropagation()
 }
